3c0c5e478053cf27fe882f4e5fdf009a2f85403b
jmnyarega <jmnyarega@gmail.com>
2017-06-08 23:10:55 +0300

    Indicate all ativities

---
+++ activities.py
@@ -0,0 +1,35 @@
+
+class  Activities(object):
+
+    activities = {}
+    def __init__(self,CANCELED = 'canceled',PENDING = 'pending',DONE = 'done'):
+        self.CANCELED = CANCELED
+        self.PENDING  = PENDING
+        self.DONE     = DONE
+
+    def add_activity(self,title,description,date,status,user):       
+        self.activities[title]   =  [description,date,status,user]
+        print(self.activities)
+        return self.activities
+
+    def get_all_activities(self):
+        print(self.activities)
+        return self.activities
+
+    def delete_activity(self,title):
+        del self.activities[title]
+
+    def update_activity(self,title,description,date,status,user):
+        self.activities[title]  =    [description,date,status,user]
+
+    def set_canceled(self,title):
+        self.activities[title][2] = self.CANCELED
+
+    def set_pending(self,title):
+        self.activities[title][2] = self.PENDING
+
+    def set_done(self,title):
+        self.activities[title][2] = self.DONE
+
+    def get_status(self,title):
+        return self.activities[title][2]
